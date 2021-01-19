e20eea3e28795fc248087b288c43a8bcedef954c
ashproghelp <ashproghelp2020@gmail.com>
2021-01-19 22:25:45 +0800

    jwt auth implemented

---
+++ Model/UserModel.cs
@@ -0,0 +1,10 @@
+namespace Model
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public string EmailAddr { get; set; }
+
+    }
+}
